d22f94167e6a22abe7eee875e36a6466be35ed57
Svibex <future.science@yandex.ru>
2022-06-16 15:04:17 +0300

    feat: add server

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,35 @@
 import React from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+            </header>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Дата</th>
+                        <th>Название</th>
+                        <th>Количество</th>
+                        <th>Расстояние</th>
+                    </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <td>11.11</td>
+                    <td>Name</td>
+                    <td>number</td>
+                    <td>length</td>
+                </tr>
+                </tbody>
+            </table>
+            <div className='pagination'>
+                <button>Previous</button>
+                <p>Current  page</p>
+                <button>Next</button>
+            </div>
+        </div>
+    );
 }
 
 export default App;
